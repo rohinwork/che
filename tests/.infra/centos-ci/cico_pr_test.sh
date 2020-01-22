50a962773b5b2797f5ea594265833a18fcecca8e
Skorik Sergey <sskoryk@redhat.com>
2020-01-22 11:15:09 +0200

    check maven version

--- tests/.infra/centos-ci/cico_pr_test.sh
+++ tests/.infra/centos-ci/cico_pr_test.sh
@@ -50,6 +50,8 @@ stop=$(date +%s)
 instal_dep_duration=$(($stop - $start))
 
 yum install --assumeyes -d1 maven
+mvn --version
+
 echo "Installing all dependencies lasted $instal_dep_duration seconds."
 
 yum install -y qemu-kvm libvirt libvirt-python libguestfs-tools virt-install
